0aa67328db56585961dc347f5bb1f543f27dd84e
Rob Lopez <rplopez720@gmail.com>
2023-04-03 13:57:12 -0400

    .

--- CHAP02/terraform_vars_interp/variables.tf
+++ CHAP02/terraform_vars_interp/variables.tf
@@ -11,3 +11,4 @@ variable "application_name" {
   description = "Name of the application"
 }
 
+#Only some information will vary from one stage to another, such as ahte name of the resource and the number of instances. To give more flexability to the code we write using variables. 
