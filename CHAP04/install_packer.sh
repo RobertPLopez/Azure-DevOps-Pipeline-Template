571302c1b17eedd3949f14ece008f9cdf1cd7dd4
Robert Lopez JHUAPL <rob.lopez@jhuapl.edu>
2023-04-05 11:06:34 -0400

    Packer Notes and Templates

--- CHAP04/install_packer.sh
+++ CHAP04/install_packer.sh
@@ -9,3 +9,5 @@ curl -Os https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER
 && gpg --verify packer_${PACKER_VERSION}_SHA256SUMS.sig packer_${PACKER_VERSION}_SHA256SUMS \
 && shasum -a 256 -c packer_${PACKER_VERSION}_SHA256SUMS 2>&1 | grep "${PACKER_VERSION}_linux_amd64.zip:\sOK" \
 && unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin
+
+#This packer installation script is for Linux version only
